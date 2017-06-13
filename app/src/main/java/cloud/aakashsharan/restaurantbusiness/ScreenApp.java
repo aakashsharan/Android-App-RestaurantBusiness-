6ae060067aa8efdae2b8eea892891d8894ef7146
Aakash Sharan <akisharan@hotmail.com>
2017-06-12 22:51:03 -0500

    final app commit

---
+++ app/src/main/java/cloud/aakashsharan/restaurantbusiness/ScreenApp.java
@@ -0,0 +1,13 @@
+package cloud.aakashsharan.restaurantbusiness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScreenApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_app);
+    }
+}
